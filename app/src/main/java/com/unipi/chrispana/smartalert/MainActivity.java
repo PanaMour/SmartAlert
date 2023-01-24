76a207c1d994c73bfed368d9cbc4010fc0555af4
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-01-25 00:07:40 +0200

    Started working on Location

--- app/src/main/java/com/unipi/chrispana/smartalert/MainActivity.java
+++ app/src/main/java/com/unipi/chrispana/smartalert/MainActivity.java
@@ -1,24 +1,137 @@
 package com.unipi.chrispana.smartalert;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
+    public static int REQUEST_PERMISSION=1;
+    LocationManager locationManager;
     Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         login = findViewById(R.id.loginbutton);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        getLocation();
+    }
+
+    /*//gets permission from user to access their location
+    public void getLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+                // Build the alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Location Services Not Enabled");
+                builder.setMessage("Please enable Location Services");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        // Show location settings when the user acknowledges the alert dialog
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+                });
+                Dialog alertDialog = builder.create();
+                alertDialog.setCanceledOnTouchOutside(true);
+                alertDialog.show();
+            } else {
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+            }
+        }
+    }*/
+
+    private void requestLocationPermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+            new AlertDialog.Builder(this)
+                    .setTitle("Permission Needed")
+                    .setMessage("Permission is needed to access location from your device...")
+                    .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_PERMISSION);
+                        }
+                    })
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_PERMISSION);
+                        }
+                    }).create().show();
+        }
+        else
+        {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_PERMISSION);
+        }
+    }
+
+    private void getLocation()
+    {
+        if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION))
+            {
+
+            }
+            else
+            {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_PERMISSION);
+            }
+        }
+        else {
+
+            requestLocationPermission();
+
+        }
     }
 
     public void login(View view){
         Intent intent = new Intent(this, UserAlert.class);
         startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,  int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Thanks for enabling the permission", Toast.LENGTH_SHORT).show();
+
+                //do something permission is allowed here....
+
+            } else {
+                Toast.makeText(this, "Please allow the Permission", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
     }
 }
--- app/src/main/java/com/unipi/chrispana/smartalert/UserAlert.java
+++ app/src/main/java/com/unipi/chrispana/smartalert/UserAlert.java
@@ -1,8 +1,20 @@
 package com.unipi.chrispana.smartalert;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -15,13 +27,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class UserAlert extends AppCompatActivity {
+public class UserAlert extends AppCompatActivity implements LocationListener {
 
     Spinner events;
     EditText comments;
-    TextView location;
+    LocationManager locationManager;
     Button insertAlert;
-
+    String location = "";
     FirebaseAuth mAuth;
     FirebaseDatabase database;
     DatabaseReference reference;
@@ -39,26 +51,79 @@ public class UserAlert extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, eventSpinner);
         events.setAdapter(adapter);
         comments = findViewById(R.id.insertComments);
-        location = findViewById(R.id.insertLocation);
         insertAlert = findViewById(R.id.insertAlert);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        getLocation();
+    }
+
+    public void getLocation(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},123);
+            //return;
+        }
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Services Not Enabled");
+            builder.setMessage("Please enable Location Services");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(true);
+            alertDialog.show();
+        }
+        else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+            Location loc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (loc != null) {
+                location = loc.getLatitude()+ "," + loc.getLongitude();
+            }
+
+        }
     }
 
     public void insertAlert(View view){
         database = FirebaseDatabase.getInstance();
         reference = database.getReference("alerts");
 
-        String event = events.getSelectedItem().toString();
-        String comment = comments.getText().toString();
-        String userLocation = location.getText().toString();
-        String timestamp = java.time.LocalDate.now().toString();
-        String photo = "photo";
+        if (location.equals(""))
+            showMessage("Error!","An error occurred. Please try again!");
+        else {
 
-        AlertClass alertClass = new AlertClass(event,comment,userLocation,timestamp,photo);
+            String event = events.getSelectedItem().toString();
+            String comment = comments.getText().toString();
+            String timestamp = java.time.LocalDate.now().toString();
+            String photo = "photo";
 
-        reference.child("").push().setValue(alertClass);
+            AlertClass alertClass = new AlertClass(event, comment, location, timestamp, photo);
 
-        Toast.makeText(this,"Alert has been sent successfully!", Toast.LENGTH_SHORT).show();
-        comments.setText("");
+            reference.child("").push().setValue(alertClass);
+
+            Toast.makeText(this, "Alert has been sent successfully!", Toast.LENGTH_SHORT).show();
+            comments.setText("");
+        }
+
+    }
+
+    public void showMessage(String title, String text){
+        new android.app.AlertDialog.Builder(this)
+                .setCancelable(true)
+                .setTitle(title)
+                .setMessage(text)
+                .show();
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
 
     }
 }

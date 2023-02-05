f00776477f6f3231b98518c149fcd6d552f2bea7
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-02-05 14:56:58 +0200

    Created View Statistics

---
+++ app/src/main/java/com/unipi/chrispana/smartalert/ViewStatistics.java
@@ -0,0 +1,14 @@
+package com.unipi.chrispana.smartalert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewStatistics extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_statistics);
+    }
+}
